2a6e079a009cabc665433341b284b6cc74726eb4
Jeff Padgett <jeff_padgett@hotmail.com>
2018-12-28 11:48:08 -0600

    Added verify function.  It is being called. Need to complete it.

--- luckySevens.js
+++ luckySevens.js
@@ -3,7 +3,9 @@ Creator:  Jeffrey S. Padgett
 Date Created:  Dec. 27, 2018
 Last Modified:
 */
-
+function verify() {
+  alert("verify the bet!")
+}
 
 function roll6sided() {
   var num = Math.floor(Math.random() * 6) + 1;
